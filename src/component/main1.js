87b92151ffd631408aae76e54cf5d8627e519b29
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-25 23:16:07 +0300

    add state

---
+++ src/component/main1.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import HornedBeast from './hornedBeast.js';
+
+
+class Main extends React.Component {
+
+  render() {
+    return (
+      <div>
+
+        < HornedBeast
+          image_url={'http://modernfarmer.com/wp-content/uploads/2014/08/bharal.jpg'}
+          title={'Bharal'}
+          description={"According to reputable sources, the bharal is also known as the Himalayan blue sheep due to a bluish tint to its fur. In the interest of journalism, we spent quite awhile looking at pictures of bharals online and are sad to say that this sheep is not really colored blue. But! It does have very good horns. Sheep horns tend to be wider and smoother than narrow, knobbly goat horns, and the bharal is in possession of some excellently wide and smooth horns."} />
+
+
+        < HornedBeast
+          image_url={'http://modernfarmer.com/wp-content/uploads/2014/08/addax.jpg'}
+          title={'Addax'}
+          description={"The addax is also known as the screwhorn antelope, which is a perfect name. It’s an antelope, you see, and it has horns, and the horns have swirling teeth like a screw. Good work, whoever named this animal. “Addax” is a cool name too, though. It’s basically extinct from its native Sahara, but weirdly survives well in captivity, so it’s a common sight in zoos."} />
+      </div>
+    )
+  }
+}
+
+export default Main;
