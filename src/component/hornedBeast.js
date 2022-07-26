b33103f0b2f0dece736fdad6b9a44e37d02b6850
AmaniAlsmadi <smadi.amani94@gmail.com>
2022-07-26 14:52:02 +0300

    add state

--- src/component/hornedBeast.js
+++ src/component/hornedBeast.js
@@ -1,36 +1,36 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class HornedBeast extends React.Component {
-    constructor(props) {
+    constructor(props){
         super(props);
         this.state = {
-            clickCounter: 0
+            counter: 0 
         }
     }
 
-    handelClick = () => {
-        this.setState({ clickCounter: this.state.clickCounter + 1 })
-    }
+handelClick = () =>{
+    this.setState({ counter: this.state.counter + 1 });
+}
 
-    render() {
-        return (
-            <div>
-                <Card className="bg-dark text-white">
-                    <Card.Img src={this.props.image_url} alt={this.props.title} onClick={this.handleClick}/>
-                    <Card.ImgOverlay>
-                        <Card.Title><h2>{this.props.title}</h2></Card.Title>
-                        <Card.Text>
-                        <p>{this.props.description}</p>
-                        </Card.Text>
-                        <Card.Text><span> ❤ {this.state.clickCounter}</span></Card.Text>
-                    </Card.ImgOverlay>
-                </Card>
-                
-            </div>
-        )
-    }
+render() {
+    return (
+        <div>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.imageUrl} alt={this.props.title} onClick={this.handelClick} />
+                <Card.Body>
+                    <Card.Title>{this.props.title} ❤ {this.state.counter} </Card.Title>
+                    <Card.Text>{this.props.description}
+                    
+                    </Card.Text>
+
+                </Card.Body>
+            </Card>
+
+        </div>
+    )
+}
 }
 
 export default HornedBeast;
--- src/component/main1.js
+++ src/component/main1.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import HornedBeast from './hornedBeast.js';
-
+import HornedBeast from './hornedBeast';
+import data from './data.json';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class Main extends React.Component {
 
   render() {
     return (
-      <div>
+      <Row xs={1} md={3} className="g-4">
 
-        < HornedBeast
-          image_url={'http://modernfarmer.com/wp-content/uploads/2014/08/bharal.jpg'}
-          title={'Bharal'}
-          description={"According to reputable sources, the bharal is also known as the Himalayan blue sheep due to a bluish tint to its fur. In the interest of journalism, we spent quite awhile looking at pictures of bharals online and are sad to say that this sheep is not really colored blue. But! It does have very good horns. Sheep horns tend to be wider and smoother than narrow, knobbly goat horns, and the bharal is in possession of some excellently wide and smooth horns."} />
+        {
+          data.map(item =>
+            <Col>
+              < HornedBeast title={item.title} imageUrl={item.image_url} description={item.description} />
+            </Col>
+          )
+        }
 
 
-        < HornedBeast
-          image_url={'http://modernfarmer.com/wp-content/uploads/2014/08/addax.jpg'}
-          title={'Addax'}
-          description={"The addax is also known as the screwhorn antelope, which is a perfect name. It’s an antelope, you see, and it has horns, and the horns have swirling teeth like a screw. Good work, whoever named this animal. “Addax” is a cool name too, though. It’s basically extinct from its native Sahara, but weirdly survives well in captivity, so it’s a common sight in zoos."} />
-      </div>
+      </Row>
     )
   }
 }
